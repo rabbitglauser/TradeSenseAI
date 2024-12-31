dece1550fff8862315f4c2eac94226ee7b269d7d
rabbitglauser <87266456+rabbitglauser@users.noreply.github.com>
2024-12-31 17:16:53 +0100

    Add initial project structure for Stock Predictor
    This commit sets up the project with a .NET Core 3.1 console application. It includes the solution file, project configuration, and autogenerated files. The basic "Hello World" program serves as a starting point.

---
+++ Stock-Predictor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Stock_Predictor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
